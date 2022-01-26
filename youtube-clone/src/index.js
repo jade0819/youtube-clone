3688f7d1ef08296245f04a74ab99e0173e68104c
jade0819 <break0819@gmail.com>
2022-01-27 06:09:58 +0900

    .

--- youtube-clone/src/index.js
+++ youtube-clone/src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// import {BrowserRouter} from 'react-router-dom';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+// import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
